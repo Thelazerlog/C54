4f9a9bb8b6197e4ba4045b49f8b82b328d3ac2df
Kerian Devillers <98911750+Thelazerlog@users.noreply.github.com>
2023-08-21 16:12:00 -0400

    SAasdada

--- Annexe1/app/src/main/java/kerian/devillers/example/annexe1/AjouterActivity.java
+++ Annexe1/app/src/main/java/kerian/devillers/example/annexe1/AjouterActivity.java
@@ -2,13 +2,68 @@ package kerian.devillers.example.annexe1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class AjouterActivity extends AppCompatActivity {
+
+    private Button btnAjouter;
+    private EditText texte;
+    private Ecouteur ec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajouter);
+
+        btnAjouter = findViewById(R.id.button);
+        texte = findViewById(R.id.memo);
+
+        ec = new Ecouteur();
+        btnAjouter.setOnClickListener(ec);
+
+    }
+
+    private class Ecouteur implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+
+            FileOutputStream fos;
+            BufferedWriter bw = null;
+            OutputStreamWriter osw;
+            try {
+                fos = openFileOutput("memos.txt", Context.MODE_APPEND);
+                osw = new OutputStreamWriter(fos);
+                bw = new BufferedWriter(osw);
+                bw.write(String.valueOf(texte.getText()));
+                bw.newLine();
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            finally {
+                fermerflux(bw);
+            }
+            finish();
+        }
+        public void fermerflux(Writer w){
+            try {
+                w.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
--- Annexe1/app/src/main/java/kerian/devillers/example/annexe1/ListeActivity.java
+++ Annexe1/app/src/main/java/kerian/devillers/example/annexe1/ListeActivity.java
@@ -3,12 +3,62 @@ package kerian.devillers.example.annexe1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
 
 public class ListeActivity extends AppCompatActivity {
+
+    private ListView liste;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, recupererMemo());
+
+        liste = findViewById(R.id.listeMemo);
+        liste.setAdapter(arrayAdapter);
     }
+    public ArrayList<String> recupererMemo(){
+        ArrayList<String> temp = new ArrayList<>();
+        FileInputStream fis;
+        BufferedReader br = null;
+        InputStreamReader isr;
+        try {
+            fis = openFileInput("memos.txt");
+            isr = new InputStreamReader(fis);
+            br = new BufferedReader(isr);
+            String line;
+            while((line = br.readLine()) != null){
+                temp.add(line);
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            fermerflux(br);
+        }
+
+        return temp;
+    }
+    public void fermerflux(Reader r){
+        try {
+            r.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
