7260a5b18f4d70bffdf1305d1e186ef7cd3ea22e
Kerian Devillers <98911750+Thelazerlog@users.noreply.github.com>
2023-08-21 14:43:20 -0400

    fda

---
+++ Annexe1/app/src/main/java/kerian/devillers/example/annexe1/AjouterActivity.java
@@ -0,0 +1,14 @@
+package kerian.devillers.example.annexe1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AjouterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter);
+    }
+}
---
+++ Annexe1/app/src/main/java/kerian/devillers/example/annexe1/ListeActivity.java
@@ -0,0 +1,14 @@
+package kerian.devillers.example.annexe1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste);
+    }
+}
